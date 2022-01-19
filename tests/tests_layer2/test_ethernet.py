66a21026f5d608fee388c67fbaae0e1ca3a21deb
EONRaider <livewire_voodoo@protonmail.com>
2022-01-19 16:10:00 -0300

    Modify TestEthernet class
    The temporary global identifiers used for the
    tests were incorporated into the MockEthFrame
    class.
    
    - Added a fixture for the raw Ethernet frame
    - Modified tests to conform to new interface

--- tests/tests_layer2/test_ethernet.py
+++ tests/tests_layer2/test_ethernet.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python3
+# https://github.com/EONRaider/Protocols
+
+__author__ = "EONRaider @ keybase.io/eonraider"
+
+import pytest
+
 from protocols.layer2.ethernet import Ethernet
 
 
-dst_mac = "11:22:33:aa:bb:cc"
-src_mac = "ab:bc:cd:11:22:33"
-ethertype = 0x86dd
-raw_header = b"\xdc\xd9\xae\x71\xc2\xa3\x00\xc0\xca\xa8\x20\x21\x08\x00"
+class MockEthFrame:
+    dst_mac = "11:22:33:aa:bb:cc"
+    src_mac = "ab:bc:cd:11:22:33"
+    ethertype = 0x86dd
+
+
+@pytest.fixture()
+def raw_eth_header():
+    return b"\xdc\xd9\xae\x71\xc2\xa3\x00\xc0\xca\xa8\x20\x21\x08\x00"
 
 
 class TestEthernet:
@@ -14,24 +26,24 @@ class TestEthernet:
         WHEN those values are valid and correctly formatted
         THEN an instance of Ethernet must be initialized without errors
         """
-        eth_header = Ethernet(dst=dst_mac,
-                              src=src_mac,
-                              eth=ethertype)
+        eth_header = Ethernet(dst=MockEthFrame.dst_mac,
+                              src=MockEthFrame.src_mac,
+                              eth=MockEthFrame.ethertype)
 
-        assert bytes(eth_header.dst) == b'\x11"3\xaa\xbb\xcc'
-        assert bytes(eth_header.src) == b'\xab\xbc\xcd\x11"3'
-        assert eth_header.eth == ethertype
+        assert bytes(eth_header.dst) == b"\x11\x22\x33\xaa\xbb\xcc"
+        assert bytes(eth_header.src) == b"\xab\xbc\xcd\x11\x22\x33"
+        assert eth_header.eth == MockEthFrame.ethertype
 
-    def test_decode_ethernet_header(self):
+    def test_decode_ethernet_header(self, raw_eth_header):
         """
-        GIVEN a byte-string representation of an Ethernet header
+        GIVEN a byte-string representation of an Ethernet frame header
         WHEN this header is successfully decoded
         THEN an instance of Ethernet must initialize each of its
             attributes in alignment with the byte fields
         """
-        eth_header = Ethernet.decode(raw_header)
+        eth_header = Ethernet.decode(raw_eth_header)
 
-        assert eth_header.src == '00:c0:ca:a8:20:21'
-        assert eth_header.dst == 'dc:d9:ae:71:c2:a3'
+        assert eth_header.src == "00:c0:ca:a8:20:21"
+        assert eth_header.dst == "dc:d9:ae:71:c2:a3"
         assert eth_header.eth == 0x0800
         assert eth_header.encapsulated_proto == "IPv4"
