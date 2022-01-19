42d573e1cfda9f7c838033cbaaf678a61b0c80ad
EONRaider <livewire_voodoo@protonmail.com>
2022-01-19 16:11:36 -0300

    Implement ARP.decode class method

--- protocols/layer2/arp.py
+++ protocols/layer2/arp.py
@@ -11,15 +11,15 @@ from protocols import Protocol
 
 class ARP(Protocol):           # IETF RFC 826
     _fields_ = [
-        ("htype", c_uint16),   # Hardware type
-        ("ptype", c_uint16),   # Protocol type
-        ("hlen", c_uint8),     # Hardware length
-        ("plen", c_uint8),     # Protocol length
-        ("oper", c_uint16),    # Operation code
-        ("sha", c_ubyte * 6),  # Sender hardware address
-        ("spa", c_ubyte * 4),  # Sender protocol address
-        ("tha", c_ubyte * 6),  # Target hardware address
-        ("tpa", c_ubyte * 4),  # Target protocol address
+        ("_htype", c_uint16),   # Hardware type
+        ("_ptype", c_uint16),   # Protocol type
+        ("_hlen", c_uint8),     # Hardware length
+        ("_plen", c_uint8),     # Protocol length
+        ("_oper", c_uint16),    # Operation code
+        ("_sha", c_ubyte * 6),  # Sender hardware address
+        ("_spa", c_ubyte * 4),  # Sender protocol address
+        ("_tha", c_ubyte * 6),  # Target hardware address
+        ("_tpa", c_ubyte * 4),  # Target protocol address
     ]
     header_len = 28
 
@@ -43,3 +43,17 @@ class ARP(Protocol):           # IETF RFC 826
         self.spa = self.proto_addr_to_array(spa)
         self.tha = self.hdwr_to_addr_array(tha)
         self.tpa = self.proto_addr_to_array(tpa)
+
+    @classmethod
+    def decode(cls, packet: bytes):
+        header = cls.from_buffer_copy(packet)
+        header.htype = header._htype
+        header.ptype = header._ptype
+        header.hlen = header._hlen
+        header.plen = header._plen
+        header.oper = header._oper
+        header.sha = cls.addr_array_to_hdwr(header._sha)
+        header.spa = cls.array_to_proto_addr(header._spa)
+        header.tha = cls.addr_array_to_hdwr(header._tha)
+        header.tpa = cls.array_to_proto_addr(header._tpa)
+        return header
