d24ced81a8b11358f25ae62e277bd13294e6d9d8
EONRaider <livewire_voodoo@protonmail.com>
2022-01-21 11:38:49 -0300

    Refactor tests for ARP packets
    - The MockArpPacket class was converted to a
    pytest.fixture
    - Renamed the Test.Arp class to Test.ARP
    - The TestARP.test_build_arp_header
    method was modified to include the new fixture
    in the assertions

--- tests/tests_layer2/test_arp.py
+++ tests/tests_layer2/test_arp.py
@@ -8,51 +8,45 @@ import pytest
 from protocols.layer2.arp import ARP
 
 
-class MockArpPacket:
-    htype = 1
-    ptype = 0x0800
-    hlen = 6
-    plen = 4
-    oper = 2
-    sha = "00:c0:ca:11:22:33"
-    spa = "192.168.1.96"
-    tha = "dc:d9:ae:aa:bb:cc"
-    tpa = "192.168.1.254"
+@pytest.fixture
+def mock_arp_header():
+    return ARP(
+        htype=1,
+        ptype=0x0800,
+        hlen=6,
+        plen=4,
+        oper=2,
+        sha="00:c0:ca:11:22:33",
+        spa="192.168.1.96",
+        tha="dc:d9:ae:aa:bb:cc",
+        tpa="192.168.1.254"
+    )
 
 
-@pytest.fixture()
+@pytest.fixture
 def raw_arp_header():
     return b"\x00\x01\x08\x00\x06\x04\x00\x02\x00\xc0\xca\x11\x22\x33\xc0\xa8" \
            b"\x01\x60\xdc\xd9\xae\xaa\xbb\xcc\xc0\xa8\x01\xfe"
 
 
-class TestArp:
-    def test_build_arp_packet(self):
+class TestARP:
+    def test_build_arp_header(self, mock_arp_header):
         """
         GIVEN a set of attributes defining an ARP packet
         WHEN those values are valid and correctly formatted
         THEN an instance of ARP must be initialized without errors
         """
-        arp_header = ARP(htype=MockArpPacket.htype,
-                         ptype=MockArpPacket.ptype,
-                         hlen=MockArpPacket.hlen,
-                         plen=MockArpPacket.plen,
-                         oper=MockArpPacket.oper,
-                         sha=MockArpPacket.sha,
-                         spa=MockArpPacket.spa,
-                         tha=MockArpPacket.tha,
-                         tpa=MockArpPacket.tpa)
-        assert arp_header.htype == 1
-        assert arp_header.ptype == 0x0800
-        assert arp_header.hlen == 6
-        assert arp_header.plen == 4
-        assert arp_header.oper == 2
-        assert bytes(arp_header.sha) == b"\x00\xc0\xca\x11\x22\x33"
-        assert bytes(arp_header.spa) == b"\xc0\xa8\x01\x60"
-        assert bytes(arp_header.tha) == b"\xdc\xd9\xae\xaa\xbb\xcc"
-        assert bytes(arp_header.tpa) == b"\xc0\xa8\x01\xfe"
+        assert mock_arp_header.htype == 1
+        assert mock_arp_header.ptype == 0x0800
+        assert mock_arp_header.hlen == 6
+        assert mock_arp_header.plen == 4
+        assert mock_arp_header.oper == 2
+        assert bytes(mock_arp_header.sha) == b"\x00\xc0\xca\x11\x22\x33"
+        assert bytes(mock_arp_header.spa) == b"\xc0\xa8\x01\x60"
+        assert bytes(mock_arp_header.tha) == b"\xdc\xd9\xae\xaa\xbb\xcc"
+        assert bytes(mock_arp_header.tpa) == b"\xc0\xa8\x01\xfe"
 
-    def test_decode_arp_packet(self, raw_arp_header):
+    def test_decode_arp_header(self, raw_arp_header):
         """
         GIVEN a byte-string representation of an ARP packet header
         WHEN this header is successfully decoded
