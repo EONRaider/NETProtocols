c8ef480af47e8289df9cd019c06202e8d02532e4
EONRaider <livewire_voodoo@protonmail.com>
2022-01-26 11:25:52 -0300

    Add imports for classes of netprotocols.base

--- netprotocols/__init__.py
+++ netprotocols/__init__.py
@@ -1,5 +1,5 @@
-from netprotocols.protocol import Protocol
-from netprotocols.packet import Packet
+from netprotocols.base.protocol import Protocol
+from netprotocols.base.packet import Packet
 from netprotocols.layer2.ethernet import Ethernet
 from netprotocols.layer2.arp import ARP
 from netprotocols.layer3.icmp import ICMPv4, ICMPv6
