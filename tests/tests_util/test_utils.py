bad21cf6637bdfedf8b19a1595e70d3a436ebdd3
EONRaider <livewire_voodoo@protonmail.com>
2022-07-16 08:17:11 -0300

    Modify MAC address validation tests

--- tests/tests_util/test_utils.py
+++ tests/tests_util/test_utils.py
@@ -4,10 +4,10 @@
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import re
-
 import pytest
 
 import netprotocols.utils as utils
+from netprotocols.utils.exceptions import InvalidManufacturerCode
 
 
 class TestUtils:
@@ -17,7 +17,8 @@ class TestUtils:
         WHEN the arguments list is correctly passed
         THEN a random MAC address must be returned without exceptions
         """
-        valid_mac = re.compile(r"^([\da-fA-F:]){17}$")
+        valid_mac = re.compile(r"^([\dA-F]{2}:){5}([\dA-F]{2})$",
+                               flags=re.IGNORECASE)
 
         mac_addr = utils.random_mac()
         assert bool(re.match(valid_mac, mac_addr)) is True
@@ -25,9 +26,9 @@ class TestUtils:
         mac_addr = utils.random_mac(manufacturer="AA:BB:CC")
         assert bool(re.match(valid_mac, mac_addr)) is True
 
-        with pytest.raises(TypeError):
+        with pytest.raises(InvalidManufacturerCode):
             utils.random_mac(manufacturer="AA:BB")
-        with pytest.raises(TypeError):
+        with pytest.raises(InvalidManufacturerCode):
             utils.random_mac(manufacturer="AA:BB:TT")
-        with pytest.raises(TypeError):
+        with pytest.raises(InvalidManufacturerCode):
             utils.random_mac(manufacturer="AA:BB:TT:LL")
