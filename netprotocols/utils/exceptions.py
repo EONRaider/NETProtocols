56fa4ac9af70400c362a392cce07b80167eb623a
eonraider <livewire_voodoo@protonmail.com>
2022-07-17 15:02:18 -0300

    Add InvalidMACAddress exception

--- netprotocols/utils/exceptions.py
+++ netprotocols/utils/exceptions.py
@@ -8,3 +8,9 @@ class InvalidManufacturerCode(Exception):
     def __init__(self, message, code: int = 1):
         super().__init__(message)
         self.code = code
+
+
+class InvalidMACAddress(Exception):
+    def __init__(self, message, code: int = 1):
+        super().__init__(message)
+        self.code = code
