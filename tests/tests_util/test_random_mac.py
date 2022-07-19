b9225367aadef720aed605e0368cf2113dd693c1
eonraider <livewire_voodoo@protonmail.com>
2022-07-19 14:08:14 -0300

    Split and reimplement tests for random_mac

--- tests/tests_util/test_random_mac.py
+++ tests/tests_util/test_random_mac.py
@@ -5,30 +5,47 @@ __author__ = "EONRaider @ keybase.io/eonraider"
 
 import re
 import pytest
+from itertools import repeat
 
-import netprotocols.utils as utils
+from netprotocols.utils import random_mac
 from netprotocols.utils.exceptions import InvalidManufacturerCode
+from netprotocols.utils.validation.mac import mac_regex
 
 
 class TestRandomMac:
-    def test_random_mac(self):
-        """GIVEN a call to the utils.random_mac function
-        WHEN the arguments list is correctly passed
+    def test_random_mac_no_manufacturer(self):
+        """GIVEN a call to the random_mac function
+        WHEN no manufacturer code is supplied
         THEN a random MAC address must be returned without exceptions
         """
-        valid_mac = re.compile(
-            r"^([\dA-F]{2}:){5}([\dA-F]{2})$", flags=re.IGNORECASE
+        for _ in repeat(None, 100):
+            result = re.match(mac_regex, random_mac())
+            assert result
+
+    @pytest.mark.parametrize(
+        "manufacturer",
+        ["11:22:33", "AA:BB:CC", "11:BB:33", "AA:22:CC", "00:00:00"],
+    )
+    def test_random_mac_with_manufacturer(self, manufacturer):
+        """GIVEN a call to the random_mac function
+        WHEN a valid manufacturer code is supplied
+        THEN a MAC address containing the manufacturer code appended to
+            a random device ID must be returned without exceptions
+        """
+        result = re.match(mac_regex, random_mac(manufacturer)).group()
+        assert (
+                re.match(mac_regex, result).group(
+                    "manufacturer_id") == manufacturer
         )
 
-        mac_addr = utils.random_mac()
-        assert bool(re.match(valid_mac, mac_addr)) is True
-
-        mac_addr = utils.random_mac(manufacturer="AA:BB:CC")
-        assert bool(re.match(valid_mac, mac_addr)) is True
-
+    @pytest.mark.parametrize(
+        "manufacturer",
+        ["AA:BB", "AA:BB:TT", "AA:BB:TT:LL", "11:22:33:44:55:66", {}, "", 1, []]
+    )
+    def test_random_mac_invalid_manufacturer(self, manufacturer):
+        """GIVEN a call to the random_mac function
+        WHEN malformed or invalid manufacturer codes are supplied
+        THEN an InvalidManufacturerCode exception must be raised
+        """
         with pytest.raises(InvalidManufacturerCode):
-            utils.random_mac(manufacturer="AA:BB")
-        with pytest.raises(InvalidManufacturerCode):
-            utils.random_mac(manufacturer="AA:BB:TT")
-        with pytest.raises(InvalidManufacturerCode):
-            utils.random_mac(manufacturer="AA:BB:TT:LL")
+            random_mac(manufacturer)
