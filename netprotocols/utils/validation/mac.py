f104a63055e394dc72e640641e396101a0686113
eonraider <livewire_voodoo@protonmail.com>
2022-08-18 11:28:09 -0300

    Add decomposition to returned valid MAC address
    A validated MAC address is now returned as an
    object with distinct attributes for its string
    representation, device ID and OUI

--- netprotocols/utils/validation/mac.py
+++ netprotocols/utils/validation/mac.py
@@ -4,16 +4,24 @@
 __author__ = "EONRaider @ keybase.io/eonraider"
 
 import re
+from dataclasses import dataclass
 
 from netprotocols.utils.exceptions import InvalidMACAddressException
 from netprotocols.utils.validation._base import Validator
 
 mac_regex = re.compile(
-    r"^(?P<manufacturer_id>([\dA-F]{2}[:-]){2}[\dA-F]{2})"
+    r"^(?P<oui>([\dA-F]{2}[:-]){2}[\dA-F]{2})"
     r"[:-]"
     r"(?P<device_id>([\dA-F]{2}[:-]){2}[\dA-F]{2})$",
     flags=re.IGNORECASE,
 )
+
+
+@dataclass
+class _MACAddress:
+    addr: str  # String representation of the MAC address
+    device: str  # Device unique identifier
+    oui: str  # IEEE-assigned Organizationally Unique Identifier
 
 
 class ValidMACAddress(Validator):
@@ -21,9 +29,14 @@ class ValidMACAddress(Validator):
     validation of MAC addresses before assignment/manipulation."""
 
     @staticmethod
-    def validate(value: str) -> str:
+    def validate(value: str) -> _MACAddress:
         try:
-            return re.match(mac_regex, value).group()
+            valid_mac = re.match(mac_regex, value)
+            return _MACAddress(
+                addr=valid_mac.group(),
+                device=valid_mac.group("device_id"),
+                oui=valid_mac.group("oui"),
+            )
         except (TypeError, AttributeError):
             """
             TypeError: Raised if 'value' is not of type str.
